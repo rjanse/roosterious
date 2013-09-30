536999809d28372d0987826592eb4f88dbbd61ca
Ruud Greven <ruud@rgonline.nl>
2013-09-30 21:00:45 +0200

    Added a simple temporary frontpage

--- www/api/api.php
+++ www/api/api.php
@@ -5,10 +5,11 @@
 require_once(dirname(__FILE__) . "/../../inc/commons.inc.php");
 require_once(dirname(__FILE__) . "/../../inc/lib/flight/flight/Flight.php");
 
-echo "blob";
-
-Flight::route('/', function(){
-    echo 'hello world!';
+/**
+ * Get's the schedule for a specified lecturer
+ */
+Flight::route('GET /schedule/lecturer/@sLecturerId\.@sFormat', function($sLecturerId, $sFormat){
+    echo 'hello world!' . $sLecturerId . " + " . $sFormat;
 });
 
 Flight::start();
